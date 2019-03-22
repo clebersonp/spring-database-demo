5f08c1e1bd86bd0d49ff9f739589b4c1bd2f7535
Cleberson Pauluci <pauluci.cleberson@gmail.com>
2019-03-22 01:15:27 -0300

    Criados outras consultas utilizando o jdbctemplate do spring boot

--- src/main/java/com/in28minutes/database/jdbc/controller/PersonController.java
+++ src/main/java/com/in28minutes/database/jdbc/controller/PersonController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +12,7 @@ import com.in28minutes.database.jdbc.dao.PersonRepository;
 import com.in28minutes.database.jdbc.model.Person;
 
 @RestController
-@RequestMapping("/jdbc")
+@RequestMapping("/jdbc/people")
 public class PersonController {
 	
 	private PersonRepository repository;
@@ -21,8 +22,28 @@ public class PersonController {
 		this.repository = repository;
 	}
 	
-	@GetMapping("/people")
+	@GetMapping
 	public List<Person> findAll() {
 		return this.repository.findAll();
+	}
+
+	@GetMapping("/{id}")
+	public Person findById(@PathVariable Integer id) {
+		return this.repository.findById(id);
+	}
+
+	@GetMapping("/name/{name}")
+	public List<Person> findByName(@PathVariable String name) {
+		return this.repository.findByName(name);
+	}
+
+	@GetMapping("/location/{location}")
+	public List<Person> findByLocation(@PathVariable String location) {
+		return this.repository.findByLocation(location);
+	}
+
+	@GetMapping("/count")
+	public Integer count() {
+		return this.repository.count();
 	}
 }
--- src/main/java/com/in28minutes/database/jdbc/dao/PersonRepository.java
+++ src/main/java/com/in28minutes/database/jdbc/dao/PersonRepository.java
@@ -11,15 +11,34 @@ import com.in28minutes.database.jdbc.model.Person;
 
 @Repository
 public class PersonRepository {
-	
+
 	private JdbcTemplate jdbcTemplate;
 
 	@Autowired
 	public PersonRepository(JdbcTemplate jdbcTemplate) {
 		this.jdbcTemplate = jdbcTemplate;
 	}
-	
+
 	public List<Person> findAll() {
-		return this.jdbcTemplate.query("select * from person", new BeanPropertyRowMapper<Person>(Person.class));
+		return this.jdbcTemplate.query("select * from person", new BeanPropertyRowMapper<>(Person.class));
+	}
+
+	public Person findById(Integer id) {
+		return this.jdbcTemplate.queryForObject("select * from person where id = ?", new Object[] { id },
+				new BeanPropertyRowMapper<>(Person.class));
+	}
+
+	public List<Person> findByName(String name) {
+		return this.jdbcTemplate.query("select * from person where lower(name) like ?", new Object[] { "%" + name.toLowerCase() + "%" },
+				new BeanPropertyRowMapper<>(Person.class));
+	}
+
+	public List<Person> findByLocation(String location) {
+		return this.jdbcTemplate.query("select * from person where lower(location) like ?", new Object[] { "%" + location.toLowerCase() + "%" },
+				new BeanPropertyRowMapper<>(Person.class));
+	}
+
+	public Integer count() {
+		return this.jdbcTemplate.query("select * from person", new BeanPropertyRowMapper<>(Person.class)).size();
 	}
 }
