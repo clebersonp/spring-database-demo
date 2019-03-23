fe25d3de7c6f6e6d35c6f6bda9ec53c446907e8e
Cleberson Pauluci <pauluci.cleberson@gmail.com>
2019-03-23 18:15:49 -0300

    Adicionadas propriedades para visualizar o sql gerado pelo hibernate

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,6 @@
+/*
+ Usado no JdbcTemplate
+O Hibernate ira criar a tabela de acordo com o mapeamento da entidade person. Atraves da propriedade schema=update que e a default
 create table person
 (
 	id integer not null,
@@ -7,7 +10,16 @@ create table person
 	primary key(id)
 );
 
-insert into person(id, name, location, birth_date) values (1, 'cleberson', 'rua das camelias', '1985-03-02 09:30:00');
-insert into person(id, name, location, birth_date) values (2, 'bruna', 'rua das camelias', '1984-05-10 15:00:00');
-insert into person(id, name, location, birth_date) values (3, 'cristina', 'rua das camelias', '1956-03-01 07:35:00');
-insert into person(id, name, location, birth_date) values (4, 'ana', 'rua das orquideas', '1962-07-12 20:10:00');
+
+
+insert into person(id, name, location, birth_date) values (1000, 'cleberson', 'rua das camelias', '1985-03-02 09:30:00');
+insert into person(id, name, location, birth_date) values (1001, 'bruna', 'rua das camelias', '1984-05-10 15:00:00');
+insert into person(id, name, location, birth_date) values (1002, 'cristina', 'rua das camelias', '1956-03-01 07:35:00');
+insert into person(id, name, location, birth_date) values (1003, 'ana', 'rua das orquideas', '1962-07-12 20:10:00');
+*/
+
+-- Usado no Jpa
+insert into person(name, location, birth_date) values ('cleberson', 'rua das camelias', '1985-03-02 09:30:00');
+insert into person(name, location, birth_date) values ('bruna', 'rua das camelias', '1984-05-10 15:00:00');
+insert into person(name, location, birth_date) values ('cristina', 'rua das camelias', '1956-03-01 07:35:00');
+insert into person(name, location, birth_date) values ('ana', 'rua das orquideas', '1962-07-12 20:10:00');
